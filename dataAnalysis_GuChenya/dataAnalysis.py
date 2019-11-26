aa525eca17c61bd5e3439ce8845a9486f743ef33
ChenyaGu <me_chenya@126.com>
2019-11-26 23:18:52 +0800

    modified analysis

--- dataAnalysis_GuChenya/dataAnalysis.py
+++ dataAnalysis_GuChenya/dataAnalysis.py
@@ -1,32 +1,24 @@
 import numpy as np
 
+
 def calculateSE(data):
-    standardError = np.std(data,ddof=1) / np.sqrt(len(data) - 1)
+    standardError = np.std(data, ddof=1) / np.sqrt(len(data) - 1)
     return standardError
 
+
 def calculateFirstIntentStep(goalList):
-    # 不要在函数里面转化数据 应该让函数通用
-    goalList = eval(goalList)
-    # 不用magic number，把99换成len(goalList)
-    intent1 = goalList.index(1) + 1 if 1 in goalList else 99
-    intent2 = goalList.index(2) + 1 if 2 in goalList else 99
-    # 减少if 可以用python自带的min或者max简化
-    if intent1 < intent2:
-        firstIntentStep = intent1
-    elif intent2 < intent1:
-        firstIntentStep = intent2
-    else:
-        firstIntentStep = len(goalList)
+    intent1 = goalList.index(1) + 1 if 1 in goalList else len(goalList)
+    intent2 = goalList.index(2) + 1 if 2 in goalList else len(goalList)
+    firstIntentStep = min(intent1, intent2)
     return firstIntentStep
 
 
 def calculateFirstIntentStepRatio(goalList):
-    firstIntentStepRatio = calculateFirstIntentStep(goalList) / len(eval(goalList))
+    firstIntentStepRatio = calculateFirstIntentStep(goalList) / len(goalList)
     return firstIntentStepRatio
 
 
 def calculateGoalCommit(goalList):
-    goalList = eval(goalList)
     goal1Step = [goalList.index(goalList[i]) + 1 for i in range(len(goalList)) if goalList[i] == 1]
     goal2Step = [goalList.index(goalList[i]) + 1 for i in range(len(goalList)) if goalList[i] == 2]
     numGoal1 = len(goal1Step)
@@ -39,9 +31,8 @@ def calculateGoalCommit(goalList):
         isGoalCommit = 1
     return isGoalCommit
 
-# 把这个简化 不需要算最后一个goal  直接把goallist传入算第一个intend和最后一个intend是否一致（全0的情况下这种算法也可以）
+
 def calculateFinalGoal(bean1GridX, bean1GridY, trajectory):
-    trajectory = eval(trajectory)
     finalStep = trajectory[len(trajectory) - 1]
     if finalStep[0] == bean1GridX and finalStep[1] == bean1GridY:
         finalGoal = 1
@@ -49,24 +40,19 @@ def calculateFinalGoal(bean1GridX, bean1GridY, trajectory):
         finalGoal = 2
     return finalGoal
 
-# 同上 简化 具体做法是用reversed算goallist
-def calculateFirstIntentGoalAccord(finalGoal, goalList):
+
+def calculateFirstIntentGoalAccord(goalList):
     firstIntentStep = calculateFirstIntentStep(goalList)
-    if firstIntentStep != len(eval(goalList)):
-        firstIntent = eval(goalList)[firstIntentStep - 1]
-        if firstIntent == finalGoal:
-            isFirstIntentGoalAccord = 1
-        else:
-            isFirstIntentGoalAccord = 0
-    else:
-        isFirstIntentGoalAccord = 1
+    firstIntent = goalList[firstIntentStep - 1]
+    finalGoalStep = calculateFirstIntentStep(list(reversed(goalList)))
+    finalGoal = list(reversed(goalList))[finalGoalStep - 1]
+    isFirstIntentGoalAccord = 1 if firstIntent == finalGoal else 0
     return isFirstIntentGoalAccord
+
 
 # 不需要去和机器做比较  机器没有所谓的反应时  就是一个计算时间
 # 可以把一个被试的有噪声的trial的反应时的平均值和没有的平均值做比较
 def calculateIsTimeMaxNextNoisePoint(timeList, noisePoint):
-    timeList = eval(timeList)
-    noisePoint = eval(noisePoint)
     noisePointNextStep = [i + 1 for i in noisePoint]
     timeGap = [timeList[i + 1] - timeList[i] for i in range(len(timeList) - 1)]
     maxReactTimeStep = [i + 2 for i, x in enumerate(timeGap) if x == max(timeGap)]
--- dataAnalysis_GuChenya/testDataAnalysis.py
+++ dataAnalysis_GuChenya/testDataAnalysis.py
@@ -9,9 +9,9 @@ class TestAnalysisFunctions(unittest.TestCase):
     def setUp(self):
         self.testParameter = 0
 
-    @data((('0, 0, 0, 0, 0, 0, 0, 0, 0, 0'), 1.0), \
-          (('2, 0, 1, 1, 0, 1, 1, 1, 1, 2'), 0.1), \
-          (('0, 0, 0, 0, 0, 0, 0, 2'), 1.0))
+    @data(([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 1.0), \
+          ([2, 0, 1, 1, 0, 1, 1, 1, 1, 2], 0.1), \
+          ([0, 0, 0, 0, 0, 0, 0, 2], 1.0))
     @unpack
     def testCalculateFirstIntentStepRatio(self, goalList, groundTruthRatio):
         # pass
@@ -19,10 +19,10 @@ class TestAnalysisFunctions(unittest.TestCase):
         truthValue = np.array_equal(firstIntentStepRatio, groundTruthRatio)
         self.assertTrue(truthValue)
 
-    @data((('0, 0, 0, 0, 0, 0'), 1), \
-          (('0, 0, 1, 1, 0, 1, 1, 1'), 1), \
-          (('0, 0, 1, 1, 0, 2, 2, 1'), 0), \
-          (('0, 1, 1, 0, 0, 2, 2, 0, 2'), 0))
+    @data(([0, 0, 0, 0, 0, 0], 1), \
+          ([0, 0, 1, 1, 0, 1, 1, 1], 1), \
+          ([0, 0, 1, 1, 0, 2, 2, 1], 0), \
+          ([0, 1, 1, 0, 0, 2, 2, 0, 2], 0))
     @unpack
     def testCalculateGoalCommit(self, goalList, groundTruthAnswer):
         # pass
@@ -30,8 +30,8 @@ class TestAnalysisFunctions(unittest.TestCase):
         truthValue = np.array_equal(isGoalCommit, groundTruthAnswer)
         self.assertTrue(truthValue)
 
-    @data((5, 4, ('[6, 7], [5, 7], [5, 6], [5, 5], [5, 4]'), 1), \
-          (4, 8, ('[8, 6], [8, 7], [8, 8], [9, 8], [10, 8]'), 2))
+    @data((5, 4, [[6, 7], [5, 7], [5, 6], [5, 5], [5, 4]], 1), \
+          (4, 8, [[8, 6], [8, 7], [8, 8], [9, 8], [10, 8]], 2))
     @unpack
     def testCalculateFinalGoal(self, bean1GridX, bean1GridY, trajectory, groundTruthAnswer):
         # pass
@@ -39,19 +39,19 @@ class TestAnalysisFunctions(unittest.TestCase):
         truthValue = np.array_equal(finalGoal, groundTruthAnswer)
         self.assertTrue(truthValue)
 
-    @data((1, ('0, 0, 0, 0, 0, 0, 1, 1, 2'), 1), \
-          (1, ('0, 0, 0, 0, 0, 0, 2, 1, 1'), 0), \
-          (1, ('0, 0, 0, 0, 0, 0, 0, 0, 0'), 1))
+    @data(([0, 0, 0, 0, 0, 0, 1, 2, 1], 1), \
+          ([0, 0, 0, 0, 0, 0, 2, 1, 1], 0), \
+          ([0, 0, 0, 0, 0, 0, 0, 0, 0], 1))
     @unpack
-    def testCalculateFirstIntentGoalAccord(self, finalGoal, goalList, groundTruthAnswer):
+    def testCalculateFirstIntentGoalAccord(self,goalList, groundTruthAnswer):
         # pass
-        isFirstIntentGoalAccord = calculateFirstIntentGoalAccord(finalGoal, goalList)
+        isFirstIntentGoalAccord = calculateFirstIntentGoalAccord( goalList)
         truthValue = np.array_equal(isFirstIntentGoalAccord, groundTruthAnswer)
         self.assertTrue(truthValue)
 
-    @data((('1000, 2000, 3000, 4000'), '[1,2]', 1), \
-          (('2285, 2615, 2903, 3147, 3407, 3654, 3941, 4510, 4980, 5314'), '[7]', 1), \
-          (('10, 15, 25, 40'), ('[1]'), 0))
+    @data(([1000, 2000, 3000, 4000], [1,2], 1), \
+          ([2285, 2615, 2903, 3147, 3407, 3654, 3941, 4510, 4980, 5314], [7], 1), \
+          ([10, 15, 25, 40], [1], 0))
     @unpack
     def testCalculateIsTimeMaxNextNoisePoint(self, timeList, noisePoint, groundTruthStep):
         # pass
